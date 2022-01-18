ebec949fb4809361aee4d163e994985fc0f539c9
FalkoDM <falko.demaeght@student.odisee.be>
2022-01-18 19:36:56 +0000

    updates

--- challenges/19_deploy_school_lib/create_container.sh
+++ challenges/19_deploy_school_lib/create_container.sh
@@ -1,3 +1,3 @@
 docker rm -f $(docker ps -a -q) # remove all containers
 docker build -t school-library . # build the image
-docker run -ti -p 5050:5000 -d --name school-library school-library # deploy /run in background
+docker run -ti -d --name school-library school-library # deploy /run in background
