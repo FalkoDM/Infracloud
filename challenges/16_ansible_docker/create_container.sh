0428cacf17600f88cea7f731e50252d996d78dba
FalkoDM <falko.demaeght@student.odisee.be>
2022-01-23 17:05:22 +0000

    updates

--- challenges/16_ansible_docker/create_container.sh
+++ challenges/16_ansible_docker/create_container.sh
@@ -1,4 +1,4 @@
-docker rm -f $(docker ps -a -q) # remove all containers
+#docker rm -f $(docker ps -a -q) # remove all containers
 docker image rm apache_test # remove the image
 docker build -t apache_test . # build the image
 docker run -ti -p 8888:8081 -d --name apache_test apache_test # deploy /run in background
