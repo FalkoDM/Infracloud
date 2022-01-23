15b2e8e56ffb2c3f9e9372d697a5ed0a6cd035d5
FalkoDM <falko.demaeght@student.odisee.be>
2022-01-23 15:48:05 +0000

    updates

--- challenges/18_jenkins_git/create_container_test.sh
+++ challenges/18_jenkins_git/create_container_test.sh
@@ -1,4 +1,5 @@
-docker rm -f $(docker ps -a -q) # remove all containers
+#docker rm -f $(docker ps -a -q) # remove all containers
+docker rm -f flaskproject
 docker image rm flaskproject # remove the image
 docker build -t flaskproject . # build the image
 docker run -ti -p 5050:5050 -d --name flaskproject flaskproject # deploy /run in background
